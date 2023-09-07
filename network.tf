3a6ed6499049e695004805fac323b70f83fcae5e
mbrugnoni <michael.brugnoni@gmail.com>
2023-09-07 00:59:24 -0400

    update

--- network.tf
+++ network.tf
@@ -55,6 +55,7 @@ resource "aws_vpc_endpoint" "ssmmsg" {
 resource "aws_instance" "lab_vm1" {
   ami           = "ami-051f7e7f6c2f40dc1"
   instance_type = "t2.micro"
+  iam_instance_profile = "EC2_role"
   
   # Launch into private subnet
   subnet_id = aws_subnet.private_subnet1.id
