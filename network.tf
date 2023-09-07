174920720147ebd145f4b81523e46355a567d1c1
mbrugnoni <michael.brugnoni@gmail.com>
2023-09-06 23:17:44 -0400

    update

--- network.tf
+++ network.tf
@@ -1,5 +1,15 @@
+provider "aws" {
+  region = "us-east-1" # Set your desired AWS region here
+}
+
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16" # Replace with your VPC's CIDR block
+}
+
+resource "aws_subnet" "private" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.1.0/24" # Replace with your private subnet's CIDR block
+  availability_zone       = "us-east-1a" # Replace with your desired availability zone
 }
 
 resource "aws_vpc_endpoint" "example" {
@@ -7,27 +17,25 @@ resource "aws_vpc_endpoint" "example" {
   service_name        = "com.amazonaws.us-east-1.ssm" # Point to AWS Systems Manager (SSM) service
   vpc_endpoint_type   = "Interface"
   security_group_ids  = [] # Add security groups if necessary
-  subnet_ids          = [aws_subnet.example.id] # Associate with a subnet
+  subnet_ids          = [aws_subnet.private.id] # Associate with the private subnet
 }
 
-resource "aws_route_table" "example" {
+resource "aws_route_table" "private" {
   vpc_id = aws_vpc.example.id
 
   route {
-    cidr_block = "0.0.0.0/0" # This is the default route (Internet Gateway, NAT Gateway, etc.)
+    cidr_block = "0.0.0.0/0" # Default route for private subnet (Internet Gateway, NAT Gateway, etc.)
     gateway_id = aws_vpc.example.id # Replace with the appropriate gateway ID if necessary
   }
 }
 
-resource "aws_route_table_association" "example" {
-  subnet_id      = aws_subnet.example.id # Replace with your subnet ID
-  route_table_id = aws_route_table.example.id
+resource "aws_route" "ssm_endpoint" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0" # Route all traffic to the SSM endpoint
+  vpc_endpoint_id        = aws_vpc_endpoint.example.id
 }
 
-resource "aws_subnet" "example" {
-  vpc_id                  = aws_vpc.example.id
-  cidr_block              = "10.0.0.0/24" # Replace with your subnet's CIDR block
-  availability_zone       = "us-east-1a" # Replace with your desired availability zone
-  map_public_ip_on_launch = true
+output "vpc_endpoint_dns" {
+  value = aws_vpc_endpoint.example.dns_entry.0.dns_name
 }
 
