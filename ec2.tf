839b4c3b0196ac5fb885701719de24379bafb421
mbrugnoni <michael.brugnoni@gmail.com>
2023-09-08 12:19:58 -0400

    update

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,8 @@
-# Add EC2 instance 
+# Add EC2 instance using Amazon Linux 2023 AMI - assumes you have the SSM Role created via QuickSetup
 resource "aws_instance" "lab_vm1" {
   ami           = "ami-051f7e7f6c2f40dc1"
   instance_type = "t2.micro"
   iam_instance_profile = "AmazonSSMRoleForInstancesQuickSetup"
-    
-  # Launch into private subnet
   subnet_id = aws_subnet.private_subnet1.id
   
   tags = {
